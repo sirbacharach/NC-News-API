bb02e50b23449777c4deff5014e78733ff8c7385
robert-allen <allen.robert79@gmail.com>
2023-12-11 10:03:07 +0000

    adds CORS to BE

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const express = require("express");
+const cors = require('cors');
 const { wrongPathError, psqlErrors, customErrors } = require("./errors");
 const apiRouter = require("./routes/api-router");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 app.use("/api", apiRouter);
