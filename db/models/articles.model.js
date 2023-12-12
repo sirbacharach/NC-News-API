cf60cbaab8847734c6db187ab7de18c02c2a57ab
robert-allen <allen.robert79@gmail.com>
2023-12-12 13:53:12 +0000

    removes console.log

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -48,7 +48,6 @@ exports.selectArticleById = (article_id) => {
     )
 
     .then(({ rows }) => {
-      console.log(rows)
       const article = rows;
       if (rows.length === 0)
         return Promise.reject({ status: 404, msg: "not found" });
