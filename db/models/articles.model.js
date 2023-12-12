2af8a4ee8ace95889fe99797af7013d19b6766ee
robert-allen <allen.robert79@gmail.com>
2023-12-12 13:52:16 +0000

    fixes issue with getting article with no comments, which was returning 404

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -40,7 +40,7 @@ exports.selectArticleById = (article_id) => {
   return db
     .query(
       `SELECT articles.author, articles.title, articles.article_id, articles.body, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles
-    JOIN comments
+    LEFT JOIN comments
     ON articles.article_id = comments.article_id
     WHERE articles.article_id = $1
     GROUP BY articles.article_id, comments.article_id`,
@@ -48,6 +48,7 @@ exports.selectArticleById = (article_id) => {
     )
 
     .then(({ rows }) => {
+      console.log(rows)
       const article = rows;
       if (rows.length === 0)
         return Promise.reject({ status: 404, msg: "not found" });
