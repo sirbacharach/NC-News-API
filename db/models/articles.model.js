f0c0115114996f4c06ed93d4687af306b8b869d2
robert-allen <allen.robert79@gmail.com>
2023-12-13 15:37:42 +0000

    fixes issue with fetching articles by topic

--- db/models/articles.model.js
+++ db/models/articles.model.js
@@ -1,6 +1,7 @@
 const db = require("../connection");
 const { formatArticle, convertTimestampToDate } = require("../seeds/utils");
-const format = require("pg-format")
+const format = require("pg-format");
+const { selectAllTopics } = require("./topics.model");
 
 exports.insertCommentsByArticleId = (article_id, commentToInsert) => {
   const body = commentToInsert.body;
@@ -71,39 +72,59 @@ exports.updateArticleById = (article_id, inc_votes) => {
 };
 
 exports.selectAllArticles = (topic, sort_by = "created_at", order = "ASC") => {
-  const validQueries = [
-    "author",
-    "title",
-    "article_id",
-    "topic",
-    "created_at",
-    "votes",
-    "article_img_url",
-    "comment_count",
-  ];
-  const validOrders = ["ASC", "DESC"];
 
-  if (!validQueries.includes(sort_by) || !validOrders.includes(order)) {
-    return Promise.reject({ status: 400, msg: "bad request" });
+  const topicPromise = (topic) => {
+    return selectAllTopics().then((topics) => {
+      const slugs = topics.map((topic) => {
+        return topic.slug;
+      });
+      if (topic && !slugs.includes(topic)) {
+        return Promise.reject({ status: 400, msg: "bad request" });
+      }
+    });
+  };
+
+  const queryPromise = ()=>{
+    const validQueries = [
+      "author",
+      "title",
+      "article_id",
+      "topic",
+      "created_at",
+      "votes",
+      "article_img_url",
+      "comment_count",
+    ];
+    const validOrders = ["ASC", "DESC"];
+  
+    if (!validQueries.includes(sort_by) || !validOrders.includes(order)) {
+      return Promise.reject({ status: 400, msg: "bad request" });
+    }
+  
+    let queryString = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles
+    LEFT JOIN comments
+    ON comments.article_id = articles.article_id `;
+    const queryValue = [];
+    if (topic) {
+      queryValue.push(topic);
+      queryString += `WHERE topic = $1 `;
+    }
+  
+    queryString += `GROUP BY articles.article_id `;
+  
+    if (sort_by && order && validOrders.includes(order)) {
+      queryString += `ORDER BY ${sort_by} ${order} `;
+    }
+    return db.query(queryString, queryValue).then(({ rows }) => {
+      return rows;
+    });
   }
+  
+return Promise.all([queryPromise(), topicPromise(topic)])
+.then((results)=>{
+  return results[0]
+})
 
-  let queryString = `SELECT articles.author, articles.title, articles.article_id, articles.topic, articles.created_at, articles.votes, articles.article_img_url, COUNT(comments.article_id) AS comment_count FROM articles
-  LEFT JOIN comments
-  ON comments.article_id = articles.article_id `;
-  const queryValue = [];
-  if (topic) {
-    queryValue.push(topic);
-    queryString += `WHERE topic = $1 `;
-  }
-
-  queryString += `GROUP BY articles.article_id `;
-
-  if (sort_by && order && validOrders.includes(order)) {
-    queryString += `ORDER BY ${sort_by} ${order} `;
-  }
-  return db.query(queryString, queryValue).then(({ rows }) => {
-    return rows;
-  });
 };
 
 exports.selectCommentsById = (article_id) => {
@@ -122,10 +143,16 @@ ORDER BY comment_id DESC`,
 };
 
 exports.insertArticle = (articleToPost) => {
-  const formattedArticle = formatArticle(articleToPost)
-  return db.query(format('INSERT INTO articles (author, title, body, topic) VALUES %L RETURNING *', [formattedArticle]))
+  const formattedArticle = formatArticle(articleToPost);
+  return db
+    .query(
+      format(
+        "INSERT INTO articles (author, title, body, topic) VALUES %L RETURNING *",
+        [formattedArticle]
+      )
+    )
 
-.then(({rows})=>{
-  return rows
-})
+    .then(({ rows }) => {
+      return rows;
+    });
 };
--- db/models/topics.model.js
+++ db/models/topics.model.js
@@ -1,16 +1,7 @@
 const db = require("../../db/connection");
 
-exports.selectAllTopics = (topic) => {
-  const validTopics = ["mitch", "cats", "paper"];
-  let queryString = `SELECT * FROM topics `;
-  const queryValue = [];
-  if (topic && !validTopics.includes(topic)) {
-    return Promise.reject({ status: 400, msg: "bad request" });
-  } else if (topic && validTopics.includes(topic)) {
-    queryValue.push(topic);
-    queryString += `WHERE slug = $1 `;
-  }
-  return db.query(queryString, queryValue).then((results) => {
+exports.selectAllTopics = () => {
+    return db.query(`SELECT * FROM topics`).then((results) => {
     return results.rows;
   });
 };
